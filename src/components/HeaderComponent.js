42a700a0da27c840def4e300ff87524a76cc049a
black-cofe <iamsumeet.bhar@gmail.com>
2021-01-09 11:00:33 +0530

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron} from 'reactstrap';
+import { Navbar, NavbarBrand, Jumbotron, Collapse, Nav, NavItem, NavbarToggler} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+// import { Collapse } from '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Header extends Component {
 
+    constructor(props) {
+        super(props)
+        this.toggleNav = this.toggleNav.bind(this);
+        this.state = {
+            isNavOpen : false
+        };        
+    }
+
+    toggleNav() {
+        this.setState ({
+            isNavOpen : !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <Navbar dark>
+            <div>
+                <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand classname="mr-auto" href="/">
+                            <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
+                        </NavbarBrand>
+
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"> Home </span>
+                                    </NavLink>
+                                </NavItem> 
+                                <NavItem>
+                                    <NavLink className="nav-link" to= "/aboutus">
+                                        <span className="fa fa-info fa-lg">About Us</span>
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to= "/menu">
+                                        <span className="fa fa-list fa-lg">Menu</span>
+                                    </NavLink>
+                                </NavItem>
+    
+                                <NavItem>
+                                    <NavLink className="nav-link" to= "/NavLink">
+                                        <span className="fa fa-address-card fa-lg"> About Us</span>
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
@@ -21,7 +67,7 @@ class Header extends Component {
                         </div>
                     </div>
                 </Jumbotron>
-            </React.Fragment>
+            </div>
         );
     }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-useless-constructor */
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {  Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
 
 // instead of props in RenderMenuItem(props) we just use JS properties as the parameter that are gonna come in as part of the JS object props 
-    function RenderMenuItem( {dish, onclick} ) {
+    function RenderMenuItem( {dish} ) {
 
         return (
-            <Card onClick={() => onclick(dish.id)}> 
+            <Card > 
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -23,7 +23,7 @@ import {  Card, CardImg, CardImgOverlay, CardText, CardBody,
         const menu = props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5  m-1">
-                    <RenderMenuItem dish= {dish} onclick={props.onclick} />
+                    <RenderMenuItem dish= {dish} />
                 </div>
             );
         });
